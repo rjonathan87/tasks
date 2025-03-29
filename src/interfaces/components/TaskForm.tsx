1032e47fd4a2276bca7e48efbb06acd68ee66f4c
rjonathan87 <rjonathan87@gmail.com>
2025-03-29 02:37:24 -0600

    se agregan botones con estilo y se justifican en la lista

--- src/interfaces/components/TaskForm.tsx
+++ src/interfaces/components/TaskForm.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { useTasks } from "../task-context";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons/faPlus";
 
 const TaskForm: React.FC = () => {
   const { addTask } = useTasks();
@@ -15,13 +17,21 @@ const TaskForm: React.FC = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-        placeholder="Add a new task"
-      />
-      <button type="submit">Add Task</button>
+      <div className="sm:flex items-center">
+        <input
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          placeholder="Agregar nueva"
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          <FontAwesomeIcon icon={faPlus} />
+        </button>
+      </div>
     </form>
   );
 };
--- src/interfaces/components/TaskList.tsx
+++ src/interfaces/components/TaskList.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useTasks } from "../task-context";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 const TaskList: React.FC = () => {
   const { tasks, completeTask, deleteTask } = useTasks();
@@ -9,25 +11,36 @@ const TaskList: React.FC = () => {
   };
 
   const handleDelete = (id: string) => {
-    if (window.confirm("Are you sure you want to delete this task?")) {
+    if (window.confirm("Está seguro de eliminar?")) {
       deleteTask(id);
     }
   };
 
   return (
-    <ul>
+    <ul className="list-none p-0">
       {tasks.map((task) => (
-        <li key={task.id}>
+        <li
+          key={task.id}
+          className="flex justify-between items-center py-2 border-b border-gray-200"
+        >
           <span>{task.description}</span>
-          <span>Created At: {task.createdAt.toLocaleString()}</span>
-          <span>Status: {task.status ? "Completed" : "Pending"}</span>
-          <button
-            onClick={() => handleComplete(task.id)}
-            disabled={task.status}
-          >
-            {task.status ? "Completed" : "Complete"}
-          </button>
-          <button onClick={() => handleDelete(task.id)}>Delete</button>
+          <div style={{ display: "flex", gap: "10px" }}>
+            {!task.status ? (
+              <button
+                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                onClick={() => handleComplete(task.id)}
+              >
+                <FontAwesomeIcon icon={faCheck} />
+              </button>
+            ) : (
+              <button
+                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                onClick={() => handleDelete(task.id)}
+              >
+                <FontAwesomeIcon icon={faTrash} />
+              </button>
+            )}
+          </div>
         </li>
       ))}
     </ul>
